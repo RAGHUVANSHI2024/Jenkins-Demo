7d16dff22425885574ef1902ca9cac656b5595d4
Raghuvanshi kushwah <kushwahraghuvanshi32@gmail.com>
2025-07-11 13:16:13 +0530

    jenkins demo

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args) {
+        
+        System.out.println("this is java programm");
+
+        System.out.println(new Date());
+
+
+    }
+
+}
